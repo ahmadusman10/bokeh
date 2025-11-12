cf6a3c5bc2e5f84d6c05e1786848d26dfa434167
Mateusz Paprocki <mattpap@gmail.com>
2025-11-13 00:43:46 +0100

    Fix types in models/annotations/labels.pyi

--- src/bokeh/models/annotations/labels.pyi
+++ src/bokeh/models/annotations/labels.pyi
@@ -19,7 +19,11 @@ from ...core.enums import (
     VerticalAlignType as VerticalAlign,
 )
 from ...core.has_props import abstract
-from ...core.property_aliases import BorderRadius, Padding, TextAnchor
+from ...core.property_aliases import (
+    BorderRadiusType as BorderRadius,
+    PaddingType as Padding,
+    TextAnchor,
+)
 from ...core.property_mixins import (
     BackgroundFillProps,
     BackgroundHatchProps,
@@ -94,3 +98,5 @@ class Title(TextAnnotation):
     align: TextAlign = ...
 
     standoff: float = ...
+
+    offset: float = ...
